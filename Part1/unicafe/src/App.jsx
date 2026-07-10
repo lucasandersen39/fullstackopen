076a52a2dac6090d3836925dce617a606d5eec81
lucasandersen39 <lucas.andersen39@gmail.com>
2026-07-10 11:40:29 -0300

    Apply total, average and positives (1.7)

--- Part1/unicafe/src/App.jsx
+++ Part1/unicafe/src/App.jsx
@@ -7,18 +7,28 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [total, setTotal] = useState(0)
+  const [average, setAverage] = useState(0)
+  const [positive, setPositive] = useState(0)
 
   const handleGood = () => {
     setGood(good + 1)
+    setTotal(total + 1)
+    setAverage(average + 1)
+    setPositive(positive + 1)
   }
 
   const handleNeutral = () => {
     setNeutral(neutral + 1)
+    setTotal(total + 1)
   }
 
   const handleBad = () => {
     setBad(bad + 1)
+    setTotal(total + 1)
+    setAverage(average - 1)
   }
+
   return (
     <div>
       <h2>give feedback</h2>
@@ -30,6 +40,9 @@ const App = () => {
         <p>good {good}</p>
         <p>neutral {neutral}</p>
         <p>bad {bad}</p>
+        <p>all {total}</p>
+        <p>average {average / total}</p>
+        <p>positive {(positive / total) * 100} %</p>
       </div>
     </div>
   )
