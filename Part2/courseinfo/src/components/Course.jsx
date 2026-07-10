8fcbc210da8e3dc628059ed1ced5ad798ee6f69e
lucasandersen39 <lucas.andersen39@gmail.com>
2026-07-10 14:28:20 -0300

    Refactor, create Components folder and Course.jsx file. Step 6 (2.1)

---
+++ Part2/courseinfo/src/components/Course.jsx
@@ -0,0 +1,36 @@
+
+const Header = (props) => <h1>{props.course}</h1>
+
+const Content = (props) => (
+    <div>
+        <Part part={props.parts[0]} />
+        <Part part={props.parts[1]} />
+        <Part part={props.parts[2]} />
+    </div>
+)
+
+const Part = (props) => (
+    <p>
+        {props.part.name} {props.part.exercises}
+    </p>
+)
+
+const Total = (props) => <p>Number of exercises {props.total}</p>
+
+const Course = ({ course }) => {
+    return (
+        <div>
+            <Header course={course.name} />
+            <Content parts={course.parts} />
+            <Total
+                total={
+                    course.parts[0].exercises +
+                    course.parts[1].exercises +
+                    course.parts[2].exercises
+                }
+            />
+        </div>
+    )
+}
+
+export default Course
