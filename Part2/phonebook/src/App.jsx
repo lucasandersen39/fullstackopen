bc269f6bbb1ab3dcd4dc033e69048046fc81faeb
lucasandersen39 <lucas.andersen39@gmail.com>
2026-07-10 20:07:28 -0300

    Phonebook. Step 1 (2.6)

--- Part2/phonebook/src/App.jsx
+++ Part2/phonebook/src/App.jsx
@@ -1,11 +1,42 @@
+import { useState } from 'react'
 import './App.css'
 
 function App() {
 
-  return (
-    <>
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ])
+  const [newName, setNewName] = useState('')
 
-    </>
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  const handleAddPerson = (event) => {
+    event.preventDefault()
+    const newPerson = {
+      name: newName
+    }
+
+    setPersons(persons.concat(newPerson))
+
+    setNewName('')
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={handleAddPerson}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map((person, i) => <p key={i}>{person.name}</p>)}
+    </div>
   )
 }
 
