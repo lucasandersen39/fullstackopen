ad1879e820ad624781c0922ea6334c6c52f8a1ac
lucasandersen39 <lucas.andersen39@gmail.com>
2026-07-11 12:01:53 -0300

    Phonebook. Step 6. Get data from json-server (2.11)

--- Part2/phonebook/src/App.jsx
+++ Part2/phonebook/src/App.jsx
@@ -1,17 +1,13 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
+import axios from 'axios'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import ListPersons from './components/ListPersons'
 
 function App() {
 
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [showPhones, setShowPhones] = useState('')
@@ -49,6 +45,14 @@ function App() {
     setShowPhones(event.target.value)
   }
   const showPhoneBook = (showPhones == '') ? persons : persons.filter(person => person.name.includes(showPhones))
+
+  useEffect(() => {
+    axios.get('http://localhost:3001/persons')
+      .then(response => {
+        console.log("DATA RESPONSE ", response)
+        setPersons(response.data)
+      })
+  }, []) // Con [] el useEffect se ejecuta una vez cuando se renderiza el componente
 
   return (
     <div>
