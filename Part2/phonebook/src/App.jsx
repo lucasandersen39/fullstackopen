2955301e1db54e26387a88c57aad667fc869dbb8
lucasandersen39 <lucas.andersen39@gmail.com>
2026-07-11 20:49:43 -0300

    Phonebook step 10 (2.15)

--- Part2/phonebook/src/App.jsx
+++ Part2/phonebook/src/App.jsx
@@ -20,6 +20,19 @@ function App() {
     setNewNumber(event.target.value)
   }
 
+  const handleUpdatePerson = (id, updatePerson) => {
+    const person = persons.find(p => p.id == id)
+    if (!person) {
+      alert("Person not found")
+      return
+    }
+    const personUpdate = { ...person, number: updatePerson.number }
+    personService.update(id, personUpdate)
+      .then(data => {
+        setPersons(persons.map(p => p.id !== id ? p : data))
+      })
+  }
+
   const handleAddPerson = (event) => {
     event.preventDefault()
     if (newName == '' || newNumber == '') {
@@ -28,6 +41,14 @@ function App() {
     }
     const exist = persons.find(element => element.name === newName)
     if (exist) {
+      if (window.confirm(`${exist.name} is already added to phonebook, replace the old number wit a new one?`)) {
+        const updatePerson = {
+          name: exist.name,
+          number: newNumber
+        }
+        handleUpdatePerson(exist.id, updatePerson)
+        return
+      }
       alert(`${newName} is already added to phonebook`)
       return
     }
@@ -55,7 +76,6 @@ function App() {
       personService.deletePerson(person.id)
       setPersons(persons.filter(p => p.id !== id))
     }
-    console.log("ID DELETE", id)
   }
 
   const handleFilterChange = (event) => {
@@ -66,7 +86,6 @@ function App() {
   useEffect(() => {
     personService.getAll()
       .then(data => {
-        console.log("DATA RESPONSE ", data)
         setPersons(data)
       })
   }, []) // Con [] el useEffect se ejecuta una vez cuando se renderiza el componente
